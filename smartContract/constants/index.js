eed0e8125a286b852e6ab4779fe94b9e472224ba
raiyanmook27 <raiyan.mook27@gmail.com>
2022-10-05 16:24:32 +0000

    CryptoDev Setup

---
+++ smartContract/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x869dd2BA472f1cf5b4C3cFe582CbC19178Ce28ED";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
